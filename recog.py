89410fffa9779382d6aea58733faaa0425211e4d
aka9 <michaelcjx@yahoo.com.hk>
2020-07-07 15:43:43 +0200

    script recog.py and train.py available

--- recog.py
+++ recog.py
@@ -3,33 +3,50 @@ import argparse
 
 
 
+def main():	
 
-def main():
-    parser = argparse.ArgumentParser(description='add, modify and delete upstream nodes')
-    parser.add_argument(
-        'action', choices=['add', 'modify', 'delete'], help='action of upstream nodes, one of add, modify, delete')
-    parser.add_argument(
-        '-n', '--name', required=True, type=str, help='upstream node name')
-    parser.add_argument(
-        '-l', '--loki_id', required=False, type=int, help='loki id')
-    parser.add_argument(
-        '-p', '--port', required=False, type=int, help='port')
-    parser.add_argument(
-        '-i', '--ip_hash', required=False, type=int, choices=[0, 1], help='ip_hash')
-    parser.add_argument(
-        '-o', '--online', required=False, type=int, choices=[0, 1], help='online')
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser(description='options for training, one by one each pet')
+	parser.add_argument(
+		'-f', '--path_toimage', required=True, type=str, help='path to the image where you have the pet you want to recognize')
+	parser.add_argument(
+		'-m', '--model', required=False, type=str, help='path to previous model')
+	args = parser.parse_args()
 
-    if args.action == "add":
-        print args.name, args.loki_id, args.port, args.ip_hash, args.online
-    elif args.action == "modify":
-        _dict = {}
-        for i in ["loki_id", "port", "ip_hash", "online"]:
-            if getattr(args, i) is not None:
-                _dict[i] = getattr(args, i)
-        print args.name, _dict
-    elif args.action == "delete":
-        print args.name
+	try:
+		with open('match.json','r') as fp: match = json.load(fp)
+	except:
+		print('No model so can\' recognize anything')
+		return 
+
+	yolob = Yolobody()
+	yoloe = Yoloeye()
+	yolof = Yoloface()
+	haar = HaarCascadeExt()
+	face_database = Model(args.model)
+
+	im = cv2.imread(args.path_toimage)
+	ret = yolob.detect(im)
+	if not len(ret): print('We do not detect neither dog or cat in the {} image'.format(idx)) ; return 
+	if len(ret) > 1: print('We detected more than one dog or cat in the {} image'.format(idx)) ; return 
+	x,y,w,h = ret[0][2]
+	X,Y,W,H = cent2rect(x,y,w,h)
+	croped = im[Y:Y+H,X:X+W]
+
+	ret2 = yolof.detect(croped)
+	if not len(ret2): print('We do not detect neither dog or cat face in the {} image'.format(idx)) ; return
+	if len(ret2) > 1: print('We detected more than one dog or cat face in the {} image'.format(idx)) ; return 
+	x,y,w,h = ret2[0][2]
+	X,Y,W,H = cent2rect(x,y,w,h)
+	closeup = croped[Y:Y+H,X:X+W]
+	
+	ret3 = haar.detect(closeup,(0,0))[0]
+	if not (len(ret3)):  print('We do not detect neither dog or cat face in the {} image'.format(idx)) ; return
+	#if len(ret3) > 1: print('We detected more than one dog or cat face in the {} image'.format(idx)) ; continue
+	nX,nY,nW,nH = ret3
+
+	label,conf = face_database.prediction(closeup[nY:nY+nH,nX:nX+nW])
+
+	print("We recognize (among all the pet) that {} looks the most like the one on the picture".format(match[int(label)]))
 
 if __name__ == '__main__':
 	main()
--- train.py
+++ train.py
@@ -3,33 +3,63 @@ import argparse
 
 
 
-
 def main():
-    parser = argparse.ArgumentParser(description='add, modify and delete upstream nodes')
-    parser.add_argument(
-        'action', choices=['add', 'modify', 'delete'], help='action of upstream nodes, one of add, modify, delete')
-    parser.add_argument(
-        '-n', '--name', required=True, type=str, help='upstream node name')
-    parser.add_argument(
-        '-l', '--loki_id', required=False, type=int, help='loki id')
-    parser.add_argument(
-        '-p', '--port', required=False, type=int, help='port')
-    parser.add_argument(
-        '-i', '--ip_hash', required=False, type=int, choices=[0, 1], help='ip_hash')
-    parser.add_argument(
-        '-o', '--online', required=False, type=int, choices=[0, 1], help='online')
-    args = parser.parse_args()
 
-    if args.action == "add":
-        print args.name, args.loki_id, args.port, args.ip_hash, args.online
-    elif args.action == "modify":
-        _dict = {}
-        for i in ["loki_id", "port", "ip_hash", "online"]:
-            if getattr(args, i) is not None:
-                _dict[i] = getattr(args, i)
-        print args.name, _dict
-    elif args.action == "delete":
-        print args.name
+	parser = argparse.ArgumentParser(description='options for training, one by one each pet')
+	parser.add_argument(
+		'-f', '--folder', required=True, type=str, help='path to the folder image where you have the pet you want to recognize')
+	parser.add_argument(
+		'-n', '--name', required=True, type=str, help='pet\'s name')
+	parser.add_argument(
+		'-m', '--model', required=False, type=str, help='path to previous model')
+	args = parser.parse_args()
+	
+
+	yolob = Yolobody()
+	yoloe = Yoloeye()
+	yolof = Yoloface()
+	haar = HaarCascadeExt()
+	face_database = Model(args.model)
+
+	try:
+		with open('match.json','r') as fp: match = json.load(fp)
+	except:
+		match = {}
+	next_idx = len(match)
+
+	succ = 0
+	specimen = []
+	for idx,im_path in enumerate(os.listdir(args.folder),start=0):
+		im = cv2.imread(im_path)
+		ret = yolob.detect(im)
+		if not len(ret): print('We do not detect neither dog or cat in the {} image'.format(idx)) ; continue
+		if len(ret) > 1: print('We detected more than one dog or cat in the {} image'.format(idx)) ; continue
+		x,y,w,h = ret[0][2]
+		X,Y,W,H = cent2rect(x,y,w,h)
+		croped = im[Y:Y+H,X:X+W]
+
+		ret2 = yolof.detect(croped)
+		if not len(ret2): print('We do not detect neither dog or cat face in the {} image'.format(idx)) ; continue
+		if len(ret2) > 1: print('We detected more than one dog or cat face in the {} image'.format(idx)) ; continue
+		x,y,w,h = ret2[0][2]
+		X,Y,W,H = cent2rect(x,y,w,h)
+		closeup = croped[Y:Y+H,X:X+W]
+		
+		ret3 = haar.detect(closeup,(0,0))[0]
+		if not (len(ret3)):  print('We do not detect neither dog or cat face in the {} image'.format(idx)) ; continue
+		#if len(ret3) > 1: print('We detected more than one dog or cat face in the {} image'.format(idx)) ; continue
+		nX,nY,nW,nH = ret3
+		specimen.append(closeup[nY:nY+nH,nX:nX+nW])
+		succ += 1
+
+	print('Only {} images were taken to train'.format(succ))
+	if succ: 
+		face_database.training(specimen,next_idx)
+		match[next_idx] = args.name
+
+		with open('match.json', 'w') as fp:
+			json.dump(match, fp)
+	
 
 if __name__ == '__main__':
 	main()
